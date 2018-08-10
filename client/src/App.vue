41d537fd98f0b3cdd10028e1a11b7a288c40994b
CharlesChloe <huibikuile@qq.com>
2018-08-10 11:17:54 +0800

    地址修改

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    this.$fetch('/')
+      .then((response) => {
+        console.log(response)
+      })
+  }
 
 }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { routers } from './router'
+// import axios from 'axios'
+import {
+  post,
+  fetch,
+  patch,
+  put
+} from './http'
+// 定义全局变量
+Vue.prototype.$post = post
+Vue.prototype.$fetch = fetch
+Vue.prototype.$patch = patch
+Vue.prototype.$put = put
 
 Vue.config.productionTip = false
 
