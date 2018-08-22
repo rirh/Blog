dd09077b45467d8de964afc4b0c18988bef84b8a
XD <xd@XDdeMac-mini.local>
2018-08-22 22:39:36 +0800

    提交变化

--- client/src/views/user/pages/register/index.vue
+++ client/src/views/user/pages/register/index.vue
@@ -65,77 +65,76 @@
 </template>
 
 <script>
-import { postRegister } from '@/actions';
+import { postRegister } from "@/actions";
 
 export default {
   data() {
     return {
       formInline: {
-        user: '',
-        password: '',
+        user: "",
+        password: ""
       },
       ruleInline: {
         user: [
-          { required: true, message: '请输入邮箱', trigger: 'blur' },
-          { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' },
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { type: "email", message: "请输入正确的邮箱地址", trigger: "blur" }
         ],
-        password: [{
-          required: true,
-          message: '请输入密码',
-          trigger: 'blur',
-        },
-        {
-          type: 'string',
-          min: 6,
-          message: '密码长度不能小于6位',
-          trigger: 'blur',
-        },
-        ],
-      },
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "密码长度不能小于6位",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
     handleSubmit(name) {
-      this.$refs[name].validate((valid) => {
+      this.$refs[name].validate(valid => {
         if (valid) {
           postRegister({
             username: this.formInline.user,
-            password: this.formInline.password,
-          }).then(({
-            code, data: { msg },
-          }) => {
+            password: this.formInline.password
+          }).then(({ code, data: { msg } }) => {
             if (code === 200) {
               this.$Notice.info({
-                title: '提示',
-                desc: msg,
+                title: "提示",
+                desc: msg
               });
+              this.$router.push({ name: "login" });
             } else {
               this.$Notice.error({
-                title: '提示',
-                desc: msg,
+                title: "提示",
+                desc: msg
               });
             }
           });
         }
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
 .login-wapper {
-    background: #f4f4f4;
-    height: 100%;
-    width: 100%;
-    display:flex;
-    align-items: center;
-    justify-content: center
+  background: #f4f4f4;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.login-content{
-    padding: 2%;
-    width: 36%;
-    background: rgba(255, 255, 255, 0)!important
+.login-content {
+  padding: 2%;
+  width: 36%;
+  background: rgba(255, 255, 255, 0) !important;
 }
-
 </style>
